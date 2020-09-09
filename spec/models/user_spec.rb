c443cdda8145652f158efc2cb6a84c8aa4d0d3d9
okikiola11 <okikiola.apelehin@gmail.com>
2020-09-09 18:15:51 +0000

    Add the active model associations to the user_spec.rb Add the post_spec.rb model validations and asscoiations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,20 +16,40 @@ RSpec.describe User, type: :model do
       subject.password = 'johnny'
       expect(subject).to be_valid
     end
-
+  
     it 'is not valid without a name' do
       subject.name = nil
       expect(subject).to_not be_valid
     end
   
     it 'is not valid with name less than 3 characters' do
-      subject.name = 'jo'
+      subject.name = ''
       expect(subject).to_not be_valid
     end
-
+  
     it 'is not valid with name greater than 20 characters' do
       subject.name = 'john doe okikiola williams sam'
       expect(subject).to_not be_valid
     end
+  
+    it 'is not valid without email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+  
+    it 'is not valid without password' do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+  
   end
+  
+  context 'Active Model User Associations' do
+    it { should have_many(:posts) } 
+    it { should have_many(:comments) } 
+    it { should have_many(:likes) }
+    it { should have_many(:friendships) }  
+    it { should have_many(:inverse_friendships) } 
+  end
+
 end
