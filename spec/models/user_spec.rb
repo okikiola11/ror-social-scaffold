2c1bb123e37c1d94f2af53995faf938faa67d01c
okikiola11 <okikiola.apelehin@gmail.com>
2020-09-09 17:52:36 +0000

    Add the user_spec.rb valiadtions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,26 @@ RSpec.describe User, type: :model do
       )
     end
 
-    
+    it 'is valid with valid attributes' do
+      subject.name = 'john'
+      subject.email = 'johndoe@yahoo.com'
+      subject.password = 'johnny'
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+  
+    it 'is not valid with name less than 3 characters' do
+      subject.name = 'jo'
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid with name greater than 20 characters' do
+      subject.name = 'john doe okikiola williams sam'
+      expect(subject).to_not be_valid
+    end
   end
 end
