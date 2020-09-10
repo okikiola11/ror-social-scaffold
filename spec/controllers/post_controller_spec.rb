3f5b2b270fdb96d6f3c5b228ce6eb370708d1231
okikiola11 <okikiola.apelehin@gmail.com>
2020-09-10 09:21:37 +0000

    Add the post_spec controller methods

--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -15,5 +15,27 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-  
+  describe 'POST users#create' do
+    context 'with valid parameters' do
+      let(:post) { content: 'John doe, it\'s your first time here' }
+       
+      login_user
+      it 'creates a new post and renders index page' do
+        user = FactoryBot.create(:user)
+        
+        expect(response).to be_successful
+        expect { post "/posts_path", params: valid_params }.to change(Post, :count).by(+1)
+        expect(response).to have_http_status (:created)
+        expect(response).to redirect_to(posts_path)
+      end
+    end
+
+    context 'with invalid parameters' do
+      let(:post) { content: ''  }
+      login_user
+      it 'renders a new template' do
+        expect(response).to render_template(:index)    
+      end
+    end
+  end
 end
--- spec/factories/post.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :post do
-    content { 'kikiola, is a new user on the app' }
-    user_id { 1 }
-  end
-end
