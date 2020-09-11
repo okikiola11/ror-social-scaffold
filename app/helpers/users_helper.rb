fc2db8e3a6952f1fed973ad425a8b13c693379cc
praz <t.prazwal@gmail.com>
2020-09-11 17:40:48 +0200

    Add logic to see accept/reject invitations in user index page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,7 +7,13 @@ module UsersHelper
     end
   end
 
-  def friendship_options(user)
+  def friendship_options_index(user)
+    if current_user.friend_request_received.include?(user)
+      render 'users/partials/index_options', user: user
+    end
+  end
+
+  def friendship_options_show(user)
     if user == current_user
       render 'users/partials/show_partial'
     end
